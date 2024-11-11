a32127916eee3a03158edcbf28e486ec18d38026
Franco S. <114824405+FrancoSosaZ0206@users.noreply.github.com>
2024-11-11 17:33:14 -0300

    Update archInstall_2.sh

--- archInstall_2.sh
+++ archInstall_2.sh
@@ -165,73 +165,13 @@ git clone https://aur.archlinux.org/yay.git /yay
 cd /yay
 makepkg -si --noconfirm
 
-# Install remaining packages through yay and flatpak
+# Install remaining packages
+PACKAGES="com.mattjakeman.ExtensionManager \
+        com.discordapp.Discord \
+        org.gtk.Gtk3theme.Adwaita-dark"
+flatpak install flathub $PACKAGES -y
 
-# if vs code can't be installed with yay, do it with flatpak
-if ! yay -S visual-studio-code-bin --noconfirm; then
-  flatpak install flathub com.visualstudio.code -y
-fi
-
-yay -S davinci-resolve
-flatpak install flathub com.mattjakeman.ExtensionManager com.discordapp.Discord org.gtk.Gtk3theme.Adwaita-dark -y
-
-# Set Spanish (Latin American) as the keyboard layout
-clear
-localectl set-x11-keymap latam
-read -p "Press enter to continue..."
-
-# Enable autologin for the user
-clear
-sudo sed -i "/^\[daemon\]$/a AutomaticLoginEnable=True\nAutomaticLogin=yourusername" /etc/gdm/custom.conf
-read -p "Press enter to continue..."
-
-# Set custom keyboard shortcuts
-clear
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/']"
-read -p "Press enter to continue..."
-
-# Shortcut for Power Off
-clear
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "Power Off"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "gnome-session-quit --power-off"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Alt>F4"
-read -p "Press enter to continue..."
-
-# Shortcut for Reboot
-clear
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name "Reboot"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command "gnome-session-quit --reboot"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding "<Alt>R"
-read -p "Press enter to continue..."
-
-# Shortcut for Suspend
-clear
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ name "Suspend"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ command "systemctl suspend"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ binding "<Alt>S"
-read -p "Press enter to continue..."
-
-# Shortcut for Opening Music Player (Rhythmbox)
-clear
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ name "Open Music Player"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ command "rhythmbox"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ binding "<Super>M"
-read -p "Press enter to continue..."
-
-# Set Adwaita-dark as the GTK theme
-clear
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-read -p "Press enter to continue..."
-
-
-
-# Cleanup unnecessary gnome apps (for me)
-PACKAGES="gnome-contacts gnome-maps gnome-music \
-        gnome-weather gnome-tour gnome-system-monitor \
-        yelp totem malcontent"
-pacman -Rns $PACKAGES --noconfirm
-
-# Cleanup temporary file
+# Cleanup
 rm /temp_vars.sh
 
 if [ -f /temp_vars.sh ]; then
@@ -239,10 +179,24 @@ if [ -f /temp_vars.sh ]; then
 fi
 
 
-# Clear the screen
+
+clear
+echo "Downloading the third part of the setup script..."
+
+# fetch the 3rd part of the script into the installation
+curl -o /mnt/archInstall_3.sh https://raw.githubusercontent.com/FrancoSosaZ0206/MyArchInstallScript/main/archInstall_3.sh
+chmod +x /mnt/archInstall_3.sh
+
+
 clear
 # Print checkout message
-echo -e "\nInstallation complete! Run:\n\numount -a\nreboot\n\nEnjoy your new Arch Linux System! :)"
+echo -e "\nInstallation complete! Run:\n\n \
+umount -a\n \
+reboot\n\n \
+After booting into the new system, run\n\n \
+archInstall_3.sh\n\n \
+to perform some post-installation tweaks.\n \
+Enjoy your new Arch Linux System! :)\n\n"
 
 # Exit the installation, stopping this script
 exit 0
