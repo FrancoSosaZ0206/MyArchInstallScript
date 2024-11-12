cfe35e168a026b980d3eb00cbac7c30a45bb1701
Franco S. <114824405+FrancoSosaZ0206@users.noreply.github.com>
2024-11-11 22:56:34 -0300

    Update archInstall_3.sh
    Removed tweaks to gnome settings, as they don't work and can be easily done manually (another way to say I don't wanna deal with the errors these output :l)

--- archInstall_3.sh
+++ archInstall_3.sh
@@ -89,56 +89,6 @@ fi
 # ############################################# #
 # SECTION 17 - Configuration and Tweaks
 
-# Set Spanish (Latin American) as the keyboard layout
-clear
-localectl set-x11-keymap latam
-read -p "Press enter to continue..."
-
-# Enable autologin for the user
-clear
-sudo sed -i "/^\[daemon\]$/a AutomaticLoginEnable=True\nAutomaticLogin=${USERNAME}" /etc/gdm/custom.conf
-read -p "Press enter to continue..."
-
-# Set custom keyboard shortcuts
-clear
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/']"
-read -p "Press enter to continue..."
-
-# Shortcut for Power Off
-clear
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "Power Off"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "gnome-session-quit --power-off"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Alt>F4"
-read -p "Press enter to continue..."
-
-# Shortcut for Reboot
-clear
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name "Reboot"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command "gnome-session-quit --reboot"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding "<Alt>R"
-read -p "Press enter to continue..."
-
-# Shortcut for Suspend
-clear
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ name "Suspend"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ command "systemctl suspend"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ binding "<Alt>S"
-read -p "Press enter to continue..."
-
-# Shortcut for Opening Music Player (Rhythmbox)
-clear
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ name "Open Music Player"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ command "rhythmbox"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/ binding "<Super>M"
-read -p "Press enter to continue..."
-
-# Set Adwaita-dark as the GTK theme
-clear
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-read -p "Press enter to continue..."
-
-
-
 # Add global aliases for yt-dlp commands
 echo 'alias getMusic="yt-dlp -x --audio-format mp3 --audio-quality 0 --embed-metadata -P ~/Music -o \"%(artist)s - %(title)s.%(ext)s\""' >> ~/.bashrc
 echo 'alias getMusicWithMetadata="yt-dlp -x --audio-format mp3 --audio-quality 0 -P ~/Music"' >> ~/.bashrc
@@ -147,16 +97,7 @@ echo 'alias getMusicList="yt-dlp -x --audio-format mp3 --audio-quality 0 --embed
 
 
 # ############################################# #
-# SECTION 17 - Cleanup and Debloat
-
-# Remove the previous script:
-rm /archInstall_2.sh
-
-if [ -f /archInstall_2.sh ]; then
-    clear
-    echo "Warning: archInstall_2.sh could not be deleted."
-    read -p "Press enter to continue..."
-fi
+# SECTION 18 - Cleanup and Debloat
 
 # Remove unnecessary gnome apps (for me)
 PACKAGES="gnome-contacts gnome-maps gnome-music \
