9d90901a7566900ae80c316e7867d36d30902bf6
Franco S. <114824405+FrancoSosaZ0206@users.noreply.github.com>
2024-11-17 21:19:33 -0300

    Update archInstall_1.sh

--- archInstall_1.sh
+++ archInstall_1.sh
@@ -236,12 +236,11 @@ chmod +x /mnt/archInstall_2.sh
 # chroot into the installation and run part 2
 echo "Entering chroot environment..."
 arch-chroot /mnt /archInstall_2.sh || {
-    echo "Error during chroot setup. Aborting unmount and reboot."
-    exit 1
+	echo "Error during chroot setup. Aborting unmount and reboot."
+	exit 1
 }
 
 # After part 2:
-
 umount -R /mnt
 reboot
 
